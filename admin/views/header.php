f512cb94e6520af06c96b4b21735d7d218b7909b
PatoHacker458 <patohacker458@gmail.com>
2025-06-04 09:54:39 -0600

    refine

--- admin/views/header.php
+++ admin/views/header.php
@@ -1,34 +1,36 @@
 <!doctype html>
-<html lang="en">
-<head>
+<html lang="es"> <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="../../css/styles-index.css">
-    <title>MEDLINK ADMIN</title>
+    <link rel="stylesheet" href="../../css/styles-index.css"> <title>MEDLINK ADMIN</title>
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top shadow-sm" id="IndexHeader">
         <div class="container-fluid">
-            <a class="navbar-brand" href="/index.php">Inicio</a>
+            <a class="navbar-brand" href="/index.php" id="Logo">MEDLINK ADMIN</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="index.php">Dashboard</a>
+                        <a class="nav-link" href="/index.php">Inicio</a>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php">Dashboard</a>
+                    </li>
+                    <?php if ($web->checar_permiso('Administrador')): ?>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             Hospital
                         </a>
                         <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="consultorio.php">Consultorios</a></li>
-                        <li><a class="dropdown-item" href="especialidad.php">Especialidades</a></li>
-                        <li><a class="dropdown-item" href="cita.php">Citas</a></li>
+                            <li><a class="dropdown-item" href="consultorio.php">Consultorios</a></li>
+                            <li><a class="dropdown-item" href="especialidad.php">Especialidades</a></li>
+                            <li><a class="dropdown-item" href="cita.php">Citas</a></li>
                         </ul>
                     </li>
                     <li class="nav-item dropdown">
@@ -36,10 +38,10 @@
                             Personal
                         </a>
                         <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="usuario.php">Usuarios</a></li>
-                        <li><a class="dropdown-item" href="medico.php">Médicos</a></li>
-                        <li><a class="dropdown-item" href="staff.php">Staff</a></li>
-                        <li><a class="dropdown-item" href="paciente.php">Pacientes</a></li>
+                            <li><a class="dropdown-item" href="usuario.php">Usuarios</a></li>
+                            <li><a class="dropdown-item" href="medico.php">Médicos</a></li>
+                            <li><a class="dropdown-item" href="staff.php">Staff</a></li>
+                            <li><a class="dropdown-item" href="paciente.php">Pacientes</a></li>
                         </ul>
                     </li>
                     <li class="nav-item dropdown">
@@ -47,14 +49,25 @@
                             Administrativo
                         </a>
                         <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="rol.php">Roles</a></li>
-                        <li><a class="dropdown-item" href="permiso.php">Permisos</a></li>
+                            <li><a class="dropdown-item" href="rol.php">Roles</a></li>
+                            <li><a class="dropdown-item" href="permiso.php">Permisos</a></li>
                         </ul>
+                    </li>
+                    <?php endif; ?>
+                </ul>
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="login.php?accion=logout">
+                            <i class="bi bi-box-arrow-right"></i> Cerrar Sesión
+                        </a>
                     </li>
                 </ul>
             </div>
         </div>
     </nav>
+
+    <div style="padding-top: 70px;"> 
+        </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
--- index.php
+++ index.php
@@ -1,41 +1,4 @@
-<!doctype html>
-<html lang="es">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
-  <link rel="stylesheet" href="css/styles-index.css">
-  <title>MEDLINK</title>
-</head>
-
-<body>
-  <header>
-    <nav class="navbar navbar-expand-lg">
-      <div class="container">
-        <a class="navbar-brand" id="Logo">MedLink</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="index.php">Inicio</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="perfil.php">Mi Perfil</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="medicos.php">Médicos</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  </header>
+<?php require_once __DIR__ . '/header.php'; ?>
 
   <main id="main-content">
     <div class="image-container">
